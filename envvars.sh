6f55c038803d911066886f447cba150ab3d4e75e
Leonardo Luarte <leonardo@luarte.net>
2012-12-05 23:01:11 -0300

    Base structure and environment variables.

---
+++ envvars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export DETECTOR_ROOT="$HOME/tester/data"
+export DATABASE_SERVER="192.168.1.1"
+export DATABASE_USERNAME="routing"
+export DATABASE_PASSWORD="routing"
+export DATABASE_TYPE="Mysql"
+
